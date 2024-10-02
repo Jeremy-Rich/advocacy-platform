d6c9812ab42fcc267843dcaf722e2f59a57a51af
JEREMY RICH <JEREMY.RICH@BERJAK.COM.AU>
2024-10-03 02:54:08 +1000

    Resolve merge conflict in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,12 @@
-<<<<<<< HEAD
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
-=======
 import type { Config } from 'tailwindcss'
 import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{ts,tsx}',
   ],
   theme: {
@@ -102,4 +81,3 @@ const config: Config = {
 }
 
 export default config
->>>>>>> origin/main
