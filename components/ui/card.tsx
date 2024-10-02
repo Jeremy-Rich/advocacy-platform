e3701ac917038af0763378c38594ab69ba48925c
JEREMY RICH <JEREMY.RICH@BERJAK.COM.AU>
2024-10-03 02:47:06 +1000

    Resolve merge conflicts in button and card components

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,24 +1,17 @@
 import * as React from "react"
 
-<<<<<<< HEAD
-=======
 import { cn } from "@/lib/utils"
 
->>>>>>> origin/main
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-<<<<<<< HEAD
-    className={`rounded-lg border bg-card text-card-foreground shadow-sm ${className}`}
-=======
     className={cn(
       "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
->>>>>>> origin/main
     {...props}
   />
 ))
@@ -30,11 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-<<<<<<< HEAD
-    className={`flex flex-col space-y-1.5 p-6 ${className}`}
-=======
     className={cn("flex flex-col space-y-1.5 p-6", className)}
->>>>>>> origin/main
     {...props}
   />
 ))
@@ -46,21 +35,15 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-<<<<<<< HEAD
-    className={`text-2xl font-semibold leading-none tracking-tight ${className}`}
-=======
     className={cn(
       "text-2xl font-semibold leading-none tracking-tight",
       className
     )}
->>>>>>> origin/main
     {...props}
   />
 ))
 CardTitle.displayName = "CardTitle"
 
-<<<<<<< HEAD
-=======
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
@@ -73,18 +56,10 @@ const CardDescription = React.forwardRef<
 ))
 CardDescription.displayName = "CardDescription"
 
->>>>>>> origin/main
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-<<<<<<< HEAD
-  <div ref={ref} className={`p-6 pt-0 ${className}`} {...props} />
-))
-CardContent.displayName = "CardContent"
-
-export { Card, CardHeader, CardTitle, CardContent }
-=======
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
@@ -102,4 +77,3 @@ const CardFooter = React.forwardRef<
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
->>>>>>> origin/main
