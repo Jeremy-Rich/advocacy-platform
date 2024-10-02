59d9c99bc0dae2397467f01ba687b52babb18188
JEREMY RICH <JEREMY.RICH@BERJAK.COM.AU>
2024-10-03 02:49:19 +1000

    Resolve merge conflicts and update project files

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-<<<<<<< HEAD
 
-=======
- 
->>>>>>> origin/main
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
